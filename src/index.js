3bff93fe470bd8a8c5c091246305141823e472d6
hjs0410hc <hjs0410hc@github.com>
2022-05-06 22:54:45 +0900

    code refactoring

--- src/index.js
+++ src/index.js
@@ -1,59 +1,29 @@
 // index.js
 // This is the main entry point of our application
 
-const express = require('express');
 require('dotenv').config();
-const db=require('./db')
-const DB_HOST = process.env.DB_HOST;
-db.connect(DB_HOST);
-const models = require('./models')
+const express = require('express');
 const app = express();
 const { ApolloServer, gql } = require('apollo-server-express');
-const typeDefs = gql`
-type Query{
-    hello:String!
-    notes: [Note!]!
-    note(id:ID!): Note!
-}
+const models = require('./models')
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
 
-type Note{
-    id: ID!
-    content: String!
-    author: String!
-}
-
-type Mutation{
-    newNote(content: String!): Note!
-}
-`;
-const resolvers = {
-    Query:{
-        hello: ()=>'Hello world!!!',
-        notes: async ()=>{
-            return await models.Note.find();
-        },
-        note:async (parent,args)=>{
-            return await models.Note.findById(args.id);
-        }
-    },
-    Mutation:{
-        newNote:async (parent,args)=>{
-            return await models.Note.create({
-                content:args.content,
-                author:'Adam Scott'
-            })
-        }
-    }
-};
-
-let notes = [
-    {id:'1',content:'This is a note',author:'Adam Scott'},{id:'2',content:'This is another note',author:'Harlow Everly'},{id:'3',content:'This is 3nother note',author:'Riley Harrison'},
-];
-
-const server = new ApolloServer({typeDefs,resolvers});
-server.applyMiddleware({app,path:'/api'});
-
+const db=require('./db')
+const DB_HOST = process.env.DB_HOST;
 const port = process.env.PORT || 4000;
+db.connect(DB_HOST);
+
+// ApolloServer Init ///////////////////////////////////////
+const server = new ApolloServer({typeDefs,resolvers,
+    context:()=>{
+        return {models};
+    }
+});
+server.applyMiddleware({app,path:'/api'});
+////////////////////////////////////////////////////////////
+
+// express routes
 
 
 app.get('/',(req,res)=>{
---
+++ src/resolvers/query.js
@@ -0,0 +1,10 @@
+module.exports = {
+    hello: ()=>'Hello world!!!',
+    notes: async ({models})=>{
+        return await models.Note.find();
+    },
+    note:async (parent,args,{models})=>{
+        return await models.Note.findById(args.id);
+    }
+
+}
